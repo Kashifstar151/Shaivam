17cb1cdabc6b96ff7d80133d8c0f9907e0aa822c
shawnmarkus <shawnmarkus4200@gmail.com>
2023-12-27 23:12:09 +0530

    created dark and light color scheme

---
+++ src/Constants/globalCSS.js
@@ -0,0 +1,11 @@
+export const dark = {
+  colorscheme: 'dark',
+  backgroundColor: '#242424',
+  textColor: '#fff',
+};
+
+export const light = {
+  colorscheme: 'light',
+  backgroundColor: '#fff',
+  textColor: '#000000',
+};
