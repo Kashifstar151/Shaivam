03a411b54cdd8673d30d13199302a186207853c5
shawnmarkus <shawnmarkus4200@gmail.com>
2023-12-29 10:53:20 +0530

    added color in theme scheme

--- src/Constants/globalCSS.js
+++ src/Constants/globalCSS.js
@@ -1,11 +1,13 @@
 export const dark = {
-  colorscheme: 'dark',
-  backgroundColor: '#242424',
-  textColor: '#fff',
+    colorscheme: 'dark',
+    backgroundColor: '#242424',
+    textColor: '#fff',
+    cardBgColor: '#333333',
 };
 
 export const light = {
-  colorscheme: 'light',
-  backgroundColor: '#fff',
-  textColor: '#000000',
+    colorscheme: 'light',
+    backgroundColor: '#fff',
+    textColor: '#000000',
+    cardBgColor: '#C1554E',
 };
