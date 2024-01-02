781848cf00d3f29dce3a6552d490865aabae0b0a
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-02 16:08:18 +0530

    added theme provider

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
-import { StatusBar, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Route from './src/navigation/Route'
-import HomeScreen from './src/Screens/Home/HomeScreen'
-import { GestureHandlerRootView } from "react-native-gesture-handler"
+import React, { useEffect } from 'react';
+import { StatusBar, View, useColorScheme } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Route from './src/navigation/Route';
+import HomeScreen from './src/Screens/Home/HomeScreen';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import SQLite from 'react-native-sqlite-storage';
+import { ThemeContextProvider } from './src/Context/ThemeContext';
 
 const App = () => {
     return (
-      // <View>
-      <SafeAreaView style={{ flex: 1 }}>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-              <Route />
-          </GestureHandlerRootView>
-      </SafeAreaView>
-      // </View>
-  );
+        <ThemeContextProvider>
+            <SafeAreaView style={{ flex: 1 }}>
+                <GestureHandlerRootView style={{ flex: 1 }}>
+                    <Route />
+                </GestureHandlerRootView>
+            </SafeAreaView>
+        </ThemeContextProvider>
+    );
 };
 
 export default App
