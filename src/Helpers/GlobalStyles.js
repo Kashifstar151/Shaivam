ad246b2d5040bcb52d8336816c059bf18f93c063
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-02 16:09:47 +0530

    created dark and light theme color scheme

--- src/Helpers/GlobalStyles.js
+++ src/Helpers/GlobalStyles.js
@@ -1,3 +1,15 @@
+import { dark, light } from '../Constants/globalCSS';
+import colors from './colors';
 export const gStyles = {
-    marginTop: Platform.OS == 'ios' ? StatusBar.currentHeight : 0
-}
+    marginTop: Platform.OS == 'ios' ? StatusBar.currentHeight : 0,
+};
+
+export const dark = {
+    ...dark,
+    ...colors.dark,
+};
+
+export const light = {
+    ...light,
+    ...colors.light,
+};
