28195d90c3ce6b8106b237b1a5566479ba1534cf
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-04 19:58:29 +0530

    changes for version and for language

---
+++ localization/index.js
@@ -0,0 +1,128 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as RNLocalize from 'react-native-localize';
+import 'moment/min/locales';
+import moment from 'moment';
+import en from './en';
+import hi from './hi';
+import 'intl-pluralrules';
+
+export const AVAILABLE_LANGUAGES = {
+    en,
+    hi,
+};
+
+const STORE_LANGUAGE_KEY = 'settings.lang';
+
+const AVALAILABLE_LANG_CODES = Object.keys(AVAILABLE_LANGUAGES);
+
+const languageDetector = {
+    type: 'languageDetector',
+    async: true,
+    // init: (_services: Services, _detectorOptions: object, _i18nextOptions: InitOptions) => {
+    //     /* use services and options */
+    // },
+    init: () => {},
+    // detect: (callback: (lng: string) => void) => {
+    //     AsyncStorage.getItem('APP_LANG', (err, lng) => {
+    //         // Handle error fetching stored data or no data stored case
+    //         if (err || !lng) {
+    //             if (err) {
+    //                 console.log('Error fetching "APP_LANG" from async store', err);
+    //             } else {
+    //                 console.log(
+    //                     'No language is set, choosing the best available or English as fallback'
+    //                 );
+    //             }
+    //             const bestLng = RNLocalize.findBestLanguageTag(AVALAILABLE_LANG_CODES);
+
+    //             callback(bestLng?.languageTag ?? 'en');
+    //             return;
+    //         }
+    //         callback(lng);
+    //     });
+    // },
+
+    detect: function (callback) {
+        try {
+            //get stored language from Async storage
+            AsyncStorage.getItem(STORE_LANGUAGE_KEY).then((language) => {
+                if (language) {
+                    //if language was stored before, use this language in the app
+                    callback(language);
+                } else {
+                    //if language was not stored yet, use device's locale
+                    callback(RNLocalize.getLocales().map((locale) => locale.languageCode)[0]);
+                }
+            });
+        } catch (error) {
+            console.log('Error reading language', error);
+        }
+    },
+    cacheUserLanguage: (lng) => AsyncStorage.setItem(STORE_LANGUAGE_KEY, lng),
+
+    /*
+  
+  type: "languageDetector",
+  async: true,
+  init: () => {},
+  detect: async function (callback: (lang: string) => void) {
+    try {
+      //get stored language from Async storage
+      await AsyncStorage.getItem(STORE_LANGUAGE_KEY).then((language) => {
+        if (language) {
+          //if language was stored before, use this language in the app
+          return callback(language);
+        } else {
+          //if language was not stored yet, use device's locale
+          return callback(Localization.locale);
+        }
+      });
+    } catch (error) {
+      console.log("Error reading language", error);
+    }
+  },
+  cacheUserLanguage: async function (language: string) {
+    try {
+      //save a user's language choice in Async storage
+      await AsyncStorage.setItem(STORE_LANGUAGE_KEY, language);
+    } catch (error) {}
+  },
+  */
+};
+
+i18n.use(languageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: AVAILABLE_LANGUAGES,
+        react: {
+            useSuspense: false,
+        },
+        // interpolation: {
+        //     escapeValue: false,
+        //     format: function (value: any, format?: string, lng?: string) {
+        //         switch (format) {
+        //             case 'flags':
+        //                 if (typeof value !== 'number' || value < 1 || !Number.isInteger(value)) {
+        //                     return value;
+        //                 }
+        //                 if (lng === 'hi') {
+        //                     return [...Array(value as number)].map((_) => '🇮🇳').join(' ');
+        //                 } else {
+        //                     return [...Array(value as number)].map((_) => '🌎').join(' ');
+        //                 }
+        //             default:
+        //                 return value;
+        //         }
+        //     },
+        // },
+
+        interpolation: {
+            escapeValue: false, // not needed for react!!
+        },
+        fallbackLng: 'en',
+        defaultNS: 'common',
+    });
+
+i18n.on('languageChanged', (lng) => moment.locale(lng));
--- src/Screens/Home/HomeScreen.js
+++ src/Screens/Home/HomeScreen.js
@@ -6,8 +6,12 @@ import Header from '../../components/Header'
 import HeadingText from '../../components/HeadingText'
 import SearchInput from '../../components/SearchInput'
 import { colors } from "../../Helpers"
+import '../../../localization';
+import { useTranslation } from 'react-i18next';
+import moment from 'moment';
 
 const HomeScreen = () => {
+    const { t, i18n } = useTranslation();
     return (
         // <SafeAreaView>
 
@@ -16,19 +20,20 @@ const HomeScreen = () => {
                 <Header />
                 <SearchInput />
                 <HeadingText text={'Shaivam Exclusive'} />
-                <Text style={{ color: colors.grey3, fontSize: 12, marginTop: 5, fontWeight: '600' }}>Scroll through and check out what Shaiva, offers</Text>
+                <Text
+                    style={{ color: colors.grey3, fontSize: 12, marginTop: 5, fontWeight: '600' }}
+                >
+                    Scroll through and check out what Shaiva, offers
+                </Text>
                 <View style={{ marginVertical: 20 }}>
                     <CardComponents />
                 </View>
             </View>
-            <View style={styles.secondContainer}>
-                {/* <Text>Homepage</Text> */}
-
-            </View>
+            <View style={styles.secondContainer}>{/* <Text>Homepage</Text> */}</View>
         </View>
         // </SafeAreaView>
-    )
-}
+    );
+};
 export const styles = StyleSheet.create({
     main: { flex: 1 },
     firstContainer: { backgroundColor: '#AA4A44', height: Dimensions.get('window').height / 2.5, paddingHorizontal: 15 },
