4651079ede2da62dfae168521050ba2b5f5b4cb9
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-05 17:36:15 +0530

    added the localization of the languages

--- localization/index.js
+++ localization/index.js
@@ -4,42 +4,64 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as RNLocalize from 'react-native-localize';
 import 'moment/min/locales';
 import moment from 'moment';
-import en from './en';
-import hi from './hi';
+import { ar, as, bn, en, gu, he, hi, ja, kn, ml, od, si, ta, te, ur, pa } from './translations';
 
 export const AVAILABLE_LANGUAGES = {
-    en,
-    hi,
+    en: {
+        translation: en,
+    },
+    hi: {
+        translation: hi,
+    },
+    bn: {
+        translation: bn,
+    },
+    ar: {
+        translation: ar,
+    },
+    as: {
+        translation: as,
+    },
+    gu: {
+        translation: gu,
+    },
+    he: {
+        translation: he,
+    },
+    ja: {
+        translation: ja,
+    },
+    kn: {
+        translation: kn,
+    },
+    ml: {
+        translation: ml,
+    },
+    od: {
+        translation: od,
+    },
+    pa: {
+        translation: pa,
+    },
+    si: {
+        translation: si,
+    },
+    ta: {
+        translation: ta,
+    },
+    te: {
+        translation: te,
+    },
+    ur: {
+        translation: ur,
+    },
 };
-
 const STORE_LANGUAGE_KEY = 'settings.lang';
 
 const languageDetector = {
     type: 'languageDetector',
     async: true,
-    // init: (_services: Services, _detectorOptions: object, _i18nextOptions: InitOptions) => {
-    //     /* use services and options */
-    // },
     init: () => {},
-    // detect: (callback: (lng: string) => void) => {
-    //     AsyncStorage.getItem('APP_LANG', (err, lng) => {
-    //         // Handle error fetching stored data or no data stored case
-    //         if (err || !lng) {
-    //             if (err) {
-    //                 console.log('Error fetching "APP_LANG" from async store', err);
-    //             } else {
-    //                 console.log(
-    //                     'No language is set, choosing the best available or English as fallback'
-    //                 );
-    //             }
-    //             const bestLng = RNLocalize.findBestLanguageTag(AVALAILABLE_LANG_CODES);
-
-    //             callback(bestLng?.languageTag ?? 'en');
-    //             return;
-    //         }
-    //         callback(lng);
-    //     });
-    // },
 
     detect: function (callback) {
         try {
@@ -58,68 +80,20 @@ const languageDetector = {
         }
     },
     cacheUserLanguage: (lng) => AsyncStorage.setItem(STORE_LANGUAGE_KEY, lng),
-
-    /*
-  
-  type: "languageDetector",
-  async: true,
-  init: () => {},
-  detect: async function (callback: (lang: string) => void) {
-    try {
-      //get stored language from Async storage
-      await AsyncStorage.getItem(STORE_LANGUAGE_KEY).then((language) => {
-        if (language) {
-          //if language was stored before, use this language in the app
-          return callback(language);
-        } else {
-          //if language was not stored yet, use device's locale
-          return callback(Localization.locale);
-        }
-      });
-    } catch (error) {
-      console.log("Error reading language", error);
-    }
-  },
-  cacheUserLanguage: async function (language: string) {
-    try {
-      //save a user's language choice in Async storage
-      await AsyncStorage.setItem(STORE_LANGUAGE_KEY, language);
-    } catch (error) {}
-  },
-  */
 };
 
 i18n.use(languageDetector)
     .use(initReactI18next)
     .init({
+        compatibilityJSON: 'v3',
         resources: AVAILABLE_LANGUAGES,
         react: {
             useSuspense: false,
         },
-        // interpolation: {
-        //     escapeValue: false,
-        //     format: function (value: any, format?: string, lng?: string) {
-        //         switch (format) {
-        //             case 'flags':
-        //                 if (typeof value !== 'number' || value < 1 || !Number.isInteger(value)) {
-        //                     return value;
-        //                 }
-        //                 if (lng === 'hi') {
-        //                     return [...Array(value as number)].map((_) => '🇮🇳').join(' ');
-        //                 } else {
-        //                     return [...Array(value as number)].map((_) => '🌎').join(' ');
-        //                 }
-        //             default:
-        //                 return value;
-        //         }
-        //     },
-        // },
-
         interpolation: {
             escapeValue: false, // not needed for react!!
         },
         fallbackLng: 'en',
-        defaultNS: 'common',
     });
 
 i18n.on('languageChanged', (lng) => moment.locale(lng));
---
+++ localization/translations/index.js
@@ -0,0 +1,16 @@
+export { default as ar } from './Arabic.json';
+export { default as as } from './Assamese.json';
+export { default as bn } from './Bengali.json';
+export { default as en } from './English.json';
+export { default as gu } from './Gujarati.json';
+export { default as he } from './Hebrew.json';
+export { default as hi } from './Hindi.json';
+export { default as ja } from './Japanese.json';
+export { default as kn } from './Kannada.json';
+export { default as ml } from './Malayalam.json';
+export { default as od } from './Odia.json';
+export { default as si } from './Sinhala.json';
+export { default as ta } from './Tamil.json';
+export { default as te } from './Telugu.json';
+export { default as ur } from './Urdu.json';
+export { default as pa } from './Punjabi.json';
