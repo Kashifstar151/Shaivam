9dd052dbc737fb2cc424e2c228951ece6ee265ff
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-10 18:34:04 +0530

    translation for pann name has been fixed

--- src/Screens/Thrimurai/ThrimuraiHeadingPage/RenderTitle.js
+++ src/Screens/Thrimurai/ThrimuraiHeadingPage/RenderTitle.js
@@ -6,10 +6,69 @@ import { colors } from '../../../Helpers';
 import RenderAudios from '../RenderAudios';
 import { getSqlData } from '../../Database';
 import { ThemeContext } from '../../../Context/ThemeContext';
+import { useTranslation } from 'react-i18next';
+
+const RenderEachTitle = ({ item, index, navigation, selectedChapter, setSelectedChapter }) => {
+    const { theme } = useContext(ThemeContext);
+    const { t } = useTranslation();
+    return (
+        <>
+            <View
+                style={{
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
+                    paddingHorizontal: 25,
+                    alignItems: 'center',
+                    paddingBottom: 10,
+                    borderBottomColor: colors.grey3,
+                    borderBottomWidth: 1,
+                }}
+            >
+                <View style={{ width: '95%' }}>
+                    <Text
+                        style={
+                            selectedChapter == index
+                                ? [styles.titleText, { color: theme.textColor }]
+                                : [styles.titleText, { color: theme.textColor }]
+                        }
+                    >
+                        {t(item?.pann)}
+                    </Text>
+                </View>
+                {selectedChapter !== null && selectedChapter == index ? (
+                    <TouchableOpacity onPress={() => setSelectedChapter(null)}>
+                        {
+                            <Icon
+                                name="keyboard-arrow-down"
+                                size={24}
+                                color={theme.colorscheme === 'light' ? '#000' : colors.grey1}
+                            />
+                        }
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity onPress={() => setSelectedChapter(index)}>
+                        {
+                            <Icon
+                                name="keyboard-arrow-right"
+                                size={24}
+                                color={theme.colorscheme === 'light' ? '#000' : colors.grey1}
+                            />
+                        }
+                    </TouchableOpacity>
+                )}
+            </View>
+            {selectedChapter == index && (
+                <View style={{ marginBottom: 10 }}>
+                    {/* <FlatList renderItem={({ item, index }) => renderAudios(item, index)} data={item.songLyrics} /> */}
+                    <RenderAudios songs={item} navigation={navigation} />
+                </View>
+            )}
+        </>
+    );
+};
 
 const RenderTitle = ({ data, navigation }) => {
     let key = true;
-    const { theme } = useContext(ThemeContext);
     const database = SQLite.openDatabase({
         name: key ? 'SongsData.db' : 'main.db',
         createFromLocation: 1,
@@ -45,63 +104,21 @@ const RenderTitle = ({ data, navigation }) => {
         //     console.error("error occured in fetching data", error);
         // })
     };
-    const renderTitle = (item, index) => (
-        <>
-            <View
-                style={{
-                    justifyContent: 'space-between',
-                    flexDirection: 'row',
-                    paddingHorizontal: 25,
-                    alignItems: 'center',
-                    paddingBottom: 10,
-                    borderBottomColor: colors.grey3,
-                    borderBottomWidth: 1,
-                }}
-            >
-                <View style={{ width: '95%' }}>
-                    <Text
-                        style={
-                            selectedChapter == index
-                                ? [styles.titleText, { color: theme.textColor }]
-                                : [styles.titleText, { color: theme.textColor }]
-                        }
-                    >
-                        {item?.pann}
-                    </Text>
-                </View>
-                {selectedChapter !== null && selectedChapter == index ? (
-                    <TouchableOpacity onPress={() => setSelectedChapter(null)}>
-                        {
-                            <Icon
-                                name="keyboard-arrow-down"
-                                size={24}
-                                color={theme.colorscheme === 'light' ? '#000' : colors.grey1}
-                            />
-                        }
-                    </TouchableOpacity>
-                ) : (
-                    <TouchableOpacity onPress={() => setSelectedChapter(index)}>
-                        {
-                            <Icon
-                                name="keyboard-arrow-right"
-                                size={24}
-                                color={theme.colorscheme === 'light' ? '#000' : colors.grey1}
-                            />
-                        }
-                    </TouchableOpacity>
-                )}
-            </View>
-            {selectedChapter == index && (
-                <View style={{ marginBottom: 10 }}>
-                    {/* <FlatList renderItem={({ item, index }) => renderAudios(item, index)} data={item.songLyrics} /> */}
-                    <RenderAudios songs={item} navigation={navigation} />
-                </View>
-            )}
-        </>
-    );
+
     return (
         <View style={{ marginTop: 0 }}>
-            <FlatList data={TitleData} renderItem={({ item, index }) => renderTitle(item, index)} />
+            <FlatList
+                data={TitleData}
+                renderItem={({ item, index }) => (
+                    <RenderEachTitle
+                        item={item}
+                        index={index}
+                        navigation={navigation}
+                        selectedChapter={selectedChapter}
+                        setSelectedChapter={setSelectedChapter}
+                    />
+                )}
+            />
         </View>
     );
 };
