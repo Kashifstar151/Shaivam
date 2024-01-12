b6287a25ed97636b26785d12861d2fc386cb7b11
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-12 13:21:14 +0530

    created a static data based screen

--- src/Screens/Home/HomeScreen.js
+++ src/Screens/Home/HomeScreen.js
@@ -25,6 +25,13 @@ import '../../../localization';
 import { ThemeContext } from '../../Context/ThemeContext';
 import bgImg from '../../../assets/Images/Background.png';
 import bgImgDark from '../../../assets/Images/BackgroundCommon.png';
+import HomePlaylistCard from '../../components/HomePlaylistCard';
+import ElevatedCard from '../../components/ElevatedCard';
+import EventCard from '../../components/EventCard';
+import OmChat from './OmChat';
+import RightDirSVG from '../../components/SVGs/RightDirSVG';
+import HeadingAndView from './HeadingAndView';
+import PlaceCard from './PlaceCard';
 
 const LANGS = [
     { lngCode: 'en', label: 'English' },
@@ -94,13 +101,15 @@ const SongAndAudio = ({ item, index, theme }) => {
 };
 
 const HomeScreen = ({ navigation }) => {
-    console.log('the re-render ');
-    const { t, i18n } = useTranslation();
-    const selectedLngCode = i18n.language;
-    const setLng = (lngCode) => i18n.changeLanguage(lngCode);
+    const { t } = useTranslation();
     const { theme } = useContext(ThemeContext);
     const [compHeight, setCompHeight] = useState();
-    const [dimentionsOfText1, setDimentionsOfText1] = useState();
+    const [textInsidePlaylistCard, setTextInsidePlaylistCard] = useState(0);
+    const [playlistCardHeight, setPlaylistCardHeight] = useState(0);
+    const [dimentionsOfText1, setDimentionsOfText1] = useState({
+        width: 0,
+        height: 0,
+    });
     const handleLayout = useCallback(
         (event) => {
             const { height } = event.nativeEvent.layout;
@@ -131,8 +140,85 @@ const HomeScreen = ({ navigation }) => {
         },
     ];
 
+    const PlayList = [
+        {
+            id: 1,
+            heading: 'Thirumurais',
+            info: 'Attaveerattam and Saptavidanga Thevaram - Audi...',
+            subInfo: 'Thiruthani Swaminathan Odhuvar',
+            songCount: 46,
+            colors: theme.colorscheme === 'light' ? ['#FEE8B3', '#FDD166'] : ['#3A3A3A', '#3A3A3A'],
+            navDetail: '',
+        },
+        {
+            id: 2,
+            heading: 'Thirumurais',
+            info: 'Attaveerattam and Saptavidanga Thevaram - Audi...',
+            subInfo: 'Thiruthani Swaminathan Odhuvar',
+            songCount: 40,
+            colors: theme.colorscheme === 'light' ? ['#AFD9BB', '#60B278'] : ['#3A3A3A', '#3A3A3A'],
+            navDetail: '',
+        },
+        {
+            id: 3,
+            heading: 'Thirumurais',
+            info: 'Attaveerattam and Saptavidanga Thevaram - Audi...',
+            subInfo: 'Thiruthani Swaminathan Odhuvar',
+            songCount: 45,
+            colors: theme.colorscheme === 'light' ? ['#FEE8B3', '#FDD166'] : ['#3A3A3A', '#3A3A3A'],
+            navDetail: '',
+        },
+    ];
+
+    const eventData = [
+        {
+            date: { day: 'THU', dateNo: '06' },
+            title: 'முதல்-திருமுறை - Pradhosham',
+            timing: '8:00pm - 11:00pm',
+        },
+        {
+            date: { day: 'THU', dateNo: '06' },
+            title: 'முதல்-திருமுறை - Pradhosham',
+            timing: '8:00pm - 11:00pm',
+        },
+        {
+            date: { day: 'THU', dateNo: '06' },
+            title: 'முதல்-திருமுறை - Pradhosham',
+            timing: '8:00pm - 11:00pm',
+        },
+        {
+            date: { day: 'THU', dateNo: '06' },
+            title: 'முதல்-திருமுறை - Pradhosham',
+            timing: '8:00pm - 11:00pm',
+        },
+    ];
+
+    const nearByTempleData = [
+        {
+            img: 'https://s3-alpha-sig.figma.com/img/6700/69a2/03a54c10e1ad2d1887bbcff155403f1b?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PkT20h7qdc~YIQF~kGaGxb5qLWbWMoNT8MiSQGsZSIoaUduQhJ0yvVXL1udbmoV8cZLi-SlUnHH5AbOpoTnnQhYifWVAynjIZ~Jd8nYrdr3eXY9cwTGaqI2SL3iaff5ffwML2Vgyjqyeg8GQFc~TVs26g8s1R41X-sFWgbyp-J7YaAK6SlvFs3m6lwGKnIe8BFr1qsjom6qDOp8o~y6JgYInVyUpYtemjMMXGUrQgD0BJOrCKaKfG2t-jsW-VTrlrKHrKRe7arx-wVkde531J~IjCjednhvbsCV3e7ZL3Y4u9D7p1MEvkJe5WSxS7sarKFjY2S-qrgRPAvhmAxHfNA__',
+            templeName: 'Brahmalingeshwara',
+            address: 'Kanakapura road, Banglore',
+        },
+        {
+            img: 'https://s3-alpha-sig.figma.com/img/271a/e6ac/4dafe0139a026f994bc4f9e2718768cf?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YyIAVCbH6bvqM4W1z8nwzvYtllzoNjN0GCuB0scpWZBQXXR8miDFBtfA2j43~yxLa5S3dP9qSNcsowbRGQiHnJhbEum2SHffUrCGXCQQdGZ7mcOAOSM8WiX1t-VmNKqGTVuYqFCf-eI1Gg2xZJEyrrsxD8bT24kQJa4dgaGJoWav8M4L5TUXq57wbJ2UNA9p9-tvkvBQyGOyyN-iv1pkGsDgEto69UB-uuQI2P2rnNhrpUx020TKc34WRUAf9lTmpL29iuodm2uioh2euQLDQff5dpd0S3WBMAzR2kE6bkSS~-V1AY2Z8D8dwHX1Mkd2l0Kf9VoPSUgYTpcx0HmOGA__',
+            templeName: 'Mahadeshwara',
+            address: 'Kanakapura road, Banglore',
+        },
+        {
+            img: 'https://s3-alpha-sig.figma.com/img/6700/69a2/03a54c10e1ad2d1887bbcff155403f1b?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PkT20h7qdc~YIQF~kGaGxb5qLWbWMoNT8MiSQGsZSIoaUduQhJ0yvVXL1udbmoV8cZLi-SlUnHH5AbOpoTnnQhYifWVAynjIZ~Jd8nYrdr3eXY9cwTGaqI2SL3iaff5ffwML2Vgyjqyeg8GQFc~TVs26g8s1R41X-sFWgbyp-J7YaAK6SlvFs3m6lwGKnIe8BFr1qsjom6qDOp8o~y6JgYInVyUpYtemjMMXGUrQgD0BJOrCKaKfG2t-jsW-VTrlrKHrKRe7arx-wVkde531J~IjCjednhvbsCV3e7ZL3Y4u9D7p1MEvkJe5WSxS7sarKFjY2S-qrgRPAvhmAxHfNA__',
+            templeName: 'Brahmalingeshwara',
+            address: 'Kanakapura road, Banglore',
+        },
+    ];
+
     return (
-        <ScrollView style={{ flex: 1, overflow: 'visible' }}>
+        <ScrollView
+            style={{
+                flex: 1,
+                overflow: 'visible',
+                backgroundColor: theme.backgroundColor,
+            }}
+        >
             <View style={[styles.firstContainer]}>
                 <ImageBackground
                     source={theme.colorscheme === 'light' ? bgImg : bgImgDark}
@@ -191,9 +277,8 @@ const HomeScreen = ({ navigation }) => {
             {/* playlist 2 */}
             <View
                 style={{
-                    backgroundColor: 'red',
-                    height: 260,
-                    marginBottom: 230,
+                    // backgroundColor: 'red',
+                    height: 250,
                 }}
             >
                 <Text
@@ -203,29 +288,182 @@ const HomeScreen = ({ navigation }) => {
                             {
                                 translateX: -dimentionsOfText1.width / 2 + dimentionsOfText1.height,
                             },
-                            { translateY: (260 + dimentionsOfText1.width) / 2 },
+                            { translateY: (260 + dimentionsOfText1.width / 2) / 2 },
                             { rotate: '-90deg' },
                             { translateX: dimentionsOfText1.width / 2 },
                         ],
 
                         fontSize: 18,
                         fontWeight: 'bold',
+                        letterSpacing: 5,
+                        color: theme.textColor,
                     }}
                     onLayout={(event) => {
                         const { width, height } = event.nativeEvent.layout;
-                        setDimentionsOfText1({ width, height });
+                        setDimentionsOfText1((prev) => ({ width, height }));
                     }}
                 >
                     PLAYLIST
                 </Text>
-                <View style={{ position: 'relative', left: dimentionsOfText1.height * 2 }}>
-                    <View>
-                        <Text>box</Text>
-                    </View>
-                    <View>
-                        <Text>card</Text>
+                <View
+                    style={{
+                        position: 'relative',
+                        left: dimentionsOfText1.height * 2,
+                        width: Dimensions.get('screen').width - dimentionsOfText1.height * 2,
+                    }}
+                >
+                    <ImageBackground
+                        source={bgImg}
+                        resizeMode="cover"
+                        style={{
+                            height: textInsidePlaylistCard
+                                ? textInsidePlaylistCard + (playlistCardHeight / 2 + 30)
+                                : null,
+                            borderTopLeftRadius: 15,
+                            borderBottomLeftRadius: 15,
+                            overflow: 'hidden',
+                            padding: 15,
+                        }}
+                    >
+                        <View
+                            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+                            onLayout={(event) => {
+                                const { height } = event.nativeEvent.layout;
+                                console.log('The text layout has heighty of ===> ', height);
+                                setTextInsidePlaylistCard(height);
+                            }}
+                        >
+                            <View>
+                                <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#fff' }}>
+                                    Shaivam Playlists
+                                </Text>
+                                <Text
+                                    style={{
+                                        fontSize: 12,
+                                        fontWeight: 200,
+                                        paddingTop: 5,
+                                        color: '#fff',
+                                    }}
+                                >
+                                    We have created playlists for you
+                                </Text>
+                            </View>
+
+                            <Text
+                                style={{
+                                    fontSize: 16,
+                                    color: '#fff',
+                                }}
+                            >
+                                View all
+                            </Text>
+                        </View>
+                    </ImageBackground>
+                    <View
+                        onLayout={(event) => {
+                            const { height } = event.nativeEvent.layout;
+                            setPlaylistCardHeight(height);
+                        }}
+                        style={{
+                            marginTop: -playlistCardHeight / 2,
+                            paddingLeft: 15,
+                            overflow: 'hidden',
+                        }}
+                    >
+                        {/* <CardComponents /> */}
+                        <FlatList
+                            style={{ overflow: 'visible' }}
+                            horizontal
+                            data={PlayList}
+                            renderItem={({ item, index }) => (
+                                <HomePlaylistCard
+                                    navigation={navigation}
+                                    colors={item.colors}
+                                    heading={item.heading}
+                                    info={item.info}
+                                    subInfo={item.subInfo}
+                                    songCount={item.songCount}
+                                    navDetail={item.navDetail}
+                                    theme={{
+                                        colorScheme: theme.colorscheme,
+                                        backgroundColor: theme.backgroundColor,
+                                        textColor: theme.textColor,
+                                    }}
+                                />
+                            )}
+                        />
                     </View>
                 </View>
+            </View>
+
+            {/* upcoming events  */}
+
+            <>
+                <View style={{ paddingBottom: 15, paddingHorizontal: 15 }}>
+                    <HeadingAndView
+                        viewBtnColor={'#C1554E'}
+                        title={'Upcoming Events'}
+                        onPress={() => {}}
+                    />
+                </View>
+
+                <FlatList
+                    contentContainerStyle={{
+                        rowGap: 4,
+                        paddingBottom: 15,
+                    }}
+                    key={(item) => item?.id}
+                    data={eventData}
+                    renderItem={({ item, index }) => (
+                        <ElevatedCard>
+                            <EventCard
+                                date={item.date}
+                                timing={item.timing}
+                                title={item.title}
+                                theme={{
+                                    textColor: theme.textColor,
+                                    colorscheme: theme.colorscheme,
+                                }}
+                            />
+                        </ElevatedCard>
+                    )}
+                />
+            </>
+
+            {/* om chant section  */}
+
+            <OmChat />
+
+            {/* last section */}
+            <View style={{ paddingBottom: 100 }}>
+                <View style={{ padding: 15 }}>
+                    <HeadingAndView
+                        viewBtnColor={'#C1554E'}
+                        title={'Nearby Temples'}
+                        onPress={() => {}}
+                    />
+                </View>
+                <FlatList
+                    contentContainerStyle={{
+                        rowGap: 4,
+                        paddingBottom: 15,
+                    }}
+                    key={(item) => item?.id}
+                    data={nearByTempleData}
+                    renderItem={({ item, index }) => (
+                        <ElevatedCard>
+                            <PlaceCard
+                                img={item.img}
+                                templeName={item.templeName}
+                                address={item.address}
+                                theme={{
+                                    textColor: theme.textColor,
+                                    colorscheme: theme.colorscheme,
+                                }}
+                            />
+                        </ElevatedCard>
+                    )}
+                />
             </View>
         </ScrollView>
     );
