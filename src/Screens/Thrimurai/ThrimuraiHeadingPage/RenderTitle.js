ef8cecdc935ab5c74ae5fb10b204f9b2dbee3d0a
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-15 16:24:44 +0530

    condition based query and rendering based in flag value

--- src/Screens/Thrimurai/ThrimuraiHeadingPage/RenderTitle.js
+++ src/Screens/Thrimurai/ThrimuraiHeadingPage/RenderTitle.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import SQLite from 'react-native-sqlite-storage';
 import { colors } from '../../../Helpers';
@@ -7,73 +7,105 @@ import RenderAudios from '../RenderAudios';
 import { getSqlData } from '../../Database';
 import { ThemeContext } from '../../../Context/ThemeContext';
 import { useTranslation } from 'react-i18next';
+import { RouteTexts } from '../../../navigation/RouteText';
 
-const RenderEachTitle = ({ item, index, navigation, selectedChapter, setSelectedChapter }) => {
+const RenderEachTitle = ({
+    item,
+    index,
+    navigation,
+    selectedChapter,
+    setSelectedChapter,
+    thalam,
+    flagShowAudio,
+}) => {
+    console.log('🚀 ~ file: RenderTitle.js:21 ~ flagShowAudio:', flagShowAudio);
     const { theme } = useContext(ThemeContext);
     const { t } = useTranslation();
+    console.log('the col===>', JSON.stringify(item));
     return (
         <>
-            <View
-                style={{
-                    justifyContent: 'space-between',
-                    flexDirection: 'row',
-                    paddingHorizontal: 25,
-                    alignItems: 'center',
-                    paddingBottom: 10,
-                    borderBottomColor: colors.grey3,
-                    borderBottomWidth: 1,
-                }}
-            >
-                <View style={{ width: '95%' }}>
-                    <Text
-                        style={
-                            selectedChapter == index
-                                ? [styles.titleText, { color: theme.textColor }]
-                                : [styles.titleText, { color: theme.textColor }]
-                        }
+            {!flagShowAudio ? (
+                <>
+                    <Pressable
+                        style={{
+                            justifyContent: 'space-between',
+                            flexDirection: 'row',
+                            paddingHorizontal: 25,
+                            alignItems: 'center',
+                            paddingBottom: 10,
+                            borderBottomColor: colors.grey3,
+                            borderBottomWidth: 1,
+                        }}
+                        onPress={() => {
+                            if (thalam) {
+                                navigation.navigate(RouteTexts.THRIMURAI_SONG, {
+                                    data: item,
+                                });
+                            }
+                        }}
                     >
-                        {t(item?.pann)}
-                    </Text>
-                </View>
-                {selectedChapter !== null && selectedChapter == index ? (
-                    <TouchableOpacity onPress={() => setSelectedChapter(null)}>
-                        {
-                            <Icon
-                                name="keyboard-arrow-down"
-                                size={24}
-                                color={theme.colorscheme === 'light' ? '#000' : colors.grey1}
-                            />
-                        }
-                    </TouchableOpacity>
-                ) : (
-                    <TouchableOpacity onPress={() => setSelectedChapter(index)}>
-                        {
-                            <Icon
-                                name="keyboard-arrow-right"
-                                size={24}
-                                color={theme.colorscheme === 'light' ? '#000' : colors.grey1}
-                            />
-                        }
-                    </TouchableOpacity>
-                )}
-            </View>
-            {selectedChapter == index && (
-                <View style={{ marginBottom: 10 }}>
-                    {/* <FlatList renderItem={({ item, index }) => renderAudios(item, index)} data={item.songLyrics} /> */}
-                    <RenderAudios songs={item} navigation={navigation} />
-                </View>
+                        <View style={{ width: '95%' }}>
+                            <Text
+                                style={
+                                    selectedChapter == index
+                                        ? [styles.titleText, { color: theme.textColor }]
+                                        : [styles.titleText, { color: theme.textColor }]
+                                }
+                            >
+                                {thalam ? t(item?.title) : t(item?.pann)}
+                            </Text>
+                        </View>
+                        {!thalam ? (
+                            selectedChapter !== null && selectedChapter == index ? (
+                                <TouchableOpacity onPress={() => setSelectedChapter(null)}>
+                                    {
+                                        <Icon
+                                            name="keyboard-arrow-down"
+                                            size={24}
+                                            color={
+                                                theme.colorscheme === 'light'
+                                                    ? '#000'
+                                                    : colors.grey1
+                                            }
+                                        />
+                                    }
+                                </TouchableOpacity>
+                            ) : (
+                                <TouchableOpacity onPress={() => setSelectedChapter(index)}>
+                                    {
+                                        <Icon
+                                            name="keyboard-arrow-right"
+                                            size={24}
+                                            color={
+                                                theme.colorscheme === 'light'
+                                                    ? '#000'
+                                                    : colors.grey1
+                                            }
+                                        />
+                                    }
+                                </TouchableOpacity>
+                            )
+                        ) : null}
+                    </Pressable>
+                    {selectedChapter == index && (
+                        <View style={{ marginBottom: 10 }}>
+                            {/* <FlatList renderItem={({ item, index }) => renderAudios(item, index)} data={item.songLyrics} /> */}
+                            <RenderAudios thalam={thalam} songs={item} navigation={navigation} />
+                        </View>
+                    )}
+                </>
+            ) : (
+                <RenderAudios thalam={thalam} songs={item} navigation={navigation} />
+                // <Text>The 8 the col </Text>
             )}
         </>
     );
 };
 
-const RenderTitle = ({ data, navigation }) => {
+const RenderTitle = ({ data, navigation, thalam, ThalamHeaders, flagShowAudio }) => {
+    console.log('🚀 ~ RenderTitle ~ data:', data);
     let key = true;
-    const database = SQLite.openDatabase({
-        name: key ? 'SongsData.db' : 'main.db',
-        createFromLocation: 1,
-    });
-    // const database = SQLite.openDatabase({ name: 'SongsData.db', createFromLocation: 1 });
+
     const [selectedChapter, setSelectedChapter] = useState(null);
     const [TitleData, setTitleData] = useState([]);
 
@@ -81,41 +113,35 @@ const RenderTitle = ({ data, navigation }) => {
         getDtataFromSql();
     }, []);
     const getDtataFromSql = async () => {
-        const query = `SELECT pann, prevId,fkTrimuria FROM thirumurais where fkTrimuria=${data.prevId} and pann NOTNULL GROUP BY pann ORDER BY titleNo ASC`;
-        getSqlData(query, (callbacks) => {
+        let query;
+        if (data?.prevId < 7) {
+            query = `SELECT pann, prevId,fkTrimuria FROM thirumurais where fkTrimuria=${data.prevId} and pann NOTNULL GROUP BY pann ORDER BY titleNo ASC`;
+        } else {
+            query = `SELECT * FROM thirumurais where fkTrimuria=${data.prevId}  ORDER BY titleNo ASC`;
+        }
+        // const query = `SELECT pann, prevId,fkTrimuria FROM thirumurais where fkTrimuria=${data.prevId} and pann NOTNULL GROUP BY pann ORDER BY titleNo ASC`;
+        const query2 = `Select * from thirumurais where ${
+            ThalamHeaders == 0 ? 'country' : 'thalam'
+        }= '${data}' ORDER BY  titleNo 
+        ASC LIMIT 10 OFFSET 0`;
+        getSqlData(thalam ? query2 : query, (callbacks) => {
+            console.log('🚀 ~ getSqlData ~ callbacks:', JSON.stringify(callbacks, 0, 2));
             setTitleData(callbacks);
         });
-        // await database.transaction(tx => {
-
-        //     tx.executeSql(query, [], (_, results) => {
-        //         let arr = []
-        //         if (results?.rows?.length > 0) {
-        //             for (let i = 0; i < results?.rows?.length; i++) {
-        //                 const tableName = results.rows.item(i);
-        //                 console.log("Row data title", tableName);
-        //                 arr.push(tableName)
-        //             }
-        //         } else {
-        //             console.log('No tables found.');
-        //         }
-        //         setTitleData(arr)
-        //     })
-        // }, (error) => {
-        //     console.error("error occured in fetching data", error);
-        // })
     };
-
     return (
         <View style={{ marginTop: 0 }}>
             <FlatList
                 data={TitleData}
                 renderItem={({ item, index }) => (
                     <RenderEachTitle
+                        thalam={thalam}
                         item={item}
                         index={index}
                         navigation={navigation}
                         selectedChapter={selectedChapter}
                         setSelectedChapter={setSelectedChapter}
+                        flagShowAudio={flagShowAudio}
                     />
                 )}
             />
