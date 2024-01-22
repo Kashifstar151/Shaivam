214118db60dfcf1c6dbfff74c85cc9d90ee1fb48
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-22 16:58:02 +0530

    made the switch working for theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Image, Pressable, StatusBar, StyleSheet, Text, View } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { Image, Pressable, StatusBar, StyleSheet, Text, View, useColorScheme } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import { colors } from '../Helpers';
 import NotificationIcon from '../assets/Images/NotificationIcon.svg';
-import DarModeEnableIcon from '../assets/Images/DarkModeon.svg';
-import DarkModeDisableIcon from '../assets/Images/DarkModeOff.svg';
+import HalfMoonSVG from './SVGs/HalfMoonSVG';
+import { ThemeContext } from '../Context/ThemeContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { dark, light } from '../Helpers/GlobalStyles';
 
 const Header = () => {
+    const colorScheme = useColorScheme();
+    const [isDark, setIsDark] = useState(colorScheme === 'dark' ? true : false);
+    const { theme, setTheme } = useContext(ThemeContext);
+    const onSwitchTheme = () => {
+        setIsDark(!isDark);
+    };
+
+    useEffect(() => {
+        setTheme(isDark ? dark : light);
+        AsyncStorage.setItem('theme', colorScheme);
+    }, [isDark]);
     return (
         <View style={styles.headerContainer}>
             <View style={{ flexDirection: 'row' }}>
@@ -21,23 +34,74 @@ const Header = () => {
                     </Text>
                 </View>
             </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Pressable style={styles.notificationContainer}>
-                    <DarkModeDisableIcon />
+            <View style={styles.sideBtnContainer}>
+                <Pressable onPress={onSwitchTheme}>
+                    {/* <DarkModeDisableIcon /> */}
+                    <View
+                        style={[
+                            styles.themeSwitch,
+                            {
+                                backgroundColor:
+                                    theme.colorscheme === 'dark' ? '#3A3A3A' : '#99403A',
+                            },
+                        ]}
+                    >
+                        {/* {isDark ? (
+                            <View style={{ flexDirection: 'row-reverse', gap: 2 }}>
+                                <HalfMoonSVG
+                                    fill={theme.colorscheme === 'dark' ? '#fff' : '#99403A'}
+                                />
+                                <Text>ON</Text>
+                            </View>
+                        ) : (
+                            <View style={{ flexDirection: 'row', gap: 2 }}>
+                                <HalfMoonSVG fill={'#f00'} />
+                                <Text>OFF</Text>
+                            </View>
+                        )} */}
+
+                        <View style={{ flexDirection: isDark ? 'row-reverse' : 'row', gap: 2 }}>
+                            <HalfMoonSVG fill={theme.colorscheme === 'dark' ? '#fff' : '#E66158'} />
+                            <Text>{isDark ? 'ON' : 'OFF'}</Text>
+                        </View>
+                    </View>
                     {/* <Icon name="notifications" size={24} color='white' /> */}
                 </Pressable>
                 <Pressable style={styles.notificationContainer}>
                     <NotificationIcon />
                     {/* <Icon name="notifications" size={24} color='white' /> */}
-
                 </Pressable>
             </View>
-
         </View>
     );
 };
 export const styles = StyleSheet.create({
-    headerContainer: { paddingHorizontal: 10, paddingTop: Platform.OS == 'ios' ? StatusBar.currentHeight + 40 : 20, justifyContent: 'space-between', flexDirection: 'row' },
-    notificationContainer: { marginLeft: 10, height: 40, width: 40, borderRadius: 20, justifyContent: 'center', alignItems: 'center' }
-})
-export default Header
+    headerContainer: {
+        paddingHorizontal: 10,
+        paddingTop: Platform.OS == 'ios' ? StatusBar.currentHeight + 40 : 20,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+    },
+
+    themeSwitch: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 1000,
+    },
+
+    sideBtnContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    notificationContainer: {
+        marginLeft: 10,
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+export default Header;
