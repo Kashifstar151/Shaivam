ea963731cae0f7ee3ee8cd05840fc78d8ad2cf17
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-29 13:24:09 +0530

    changes in color of highlighted text

--- src/Screens/Thrimurai/Searchscreen/HighlightedText.js
+++ src/Screens/Thrimurai/Searchscreen/HighlightedText.js
@@ -2,30 +2,33 @@ import { useContext } from "react";
 import { Text, View } from "react-native";
 import { ThemeContext } from "../../../Context/ThemeContext";
 
-import colors from "../../../Helpers/colors";
 const HighlightedText = ({ text, highlight, lyrics }) => {
     const { theme } = useContext(ThemeContext);
     if (!text) return null;
 
     highlight = highlight.replace(/\s/g, '');
-    const escapedHighlight = highlight.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-    const regex = new RegExp(
-        `(${escapedHighlight.split("").join("\\s*")})`,
-        "gi"
-    );
+    const escapedHighlight = highlight.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const regex = new RegExp(`(${escapedHighlight.split('').join('\\s*')})`, 'gi');
     let key = 0;
     const parts = text.split(regex);
 
     return (
-        <Text style={{
-            fontFamily: 'AnekTamil-Bold',
-            fontSize: 14,
-            color: theme.textColor,
-            fontWeight: lyrics ? '400' : '700',
-        }}>
+        <Text
+            style={{
+                fontFamily: 'AnekTamil-Bold',
+                fontSize: 14,
+                color: theme.textColor,
+                fontWeight: lyrics ? '400' : '700',
+            }}
+        >
             {parts.map((part, i) =>
                 regex.test(part) ? (
-                    <Text key={key++} style={{ backgroundColor: "yellow" }}>
+                    <Text
+                        key={key++}
+                        style={{
+                            backgroundColor: theme.colorscheme === 'dark' ? '#A47300' : '#F8E3B2',
+                        }}
+                    >
                         {part}
                     </Text>
                 ) : (
