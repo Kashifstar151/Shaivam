b8ffee7744b3162021b7c40914ae997b0e9cb723
shawnmarkus <shawnmarkus4200@gmail.com>
2024-01-31 14:31:46 +0530

    theme implemented in bottomTab and more options

--- src/Screens/MoreOption/MoreOption.js
+++ src/Screens/MoreOption/MoreOption.js
@@ -1,17 +1,26 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Dimensions, FlatList, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import RBSheet from 'react-native-raw-bottom-sheet'
-import Icon from 'react-native-vector-icons/dist/MaterialIcons'
-import Background from '../../components/Background'
-import ConstantHeader from '../../components/ConstantHeader'
-import Header from '../../components/Header'
-import { colors } from '../../Helpers'
-import ChooseLanguage from './ChooseLanguage'
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import {
+    Dimensions,
+    FlatList,
+    Pressable,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
+import Background from '../../components/Background';
+import ConstantHeader from '../../components/ConstantHeader';
+import Header from '../../components/Header';
+import { colors } from '../../Helpers';
+import ChooseLanguage from './ChooseLanguage';
+import { ThemeContext } from '../../Context/ThemeContext';
 
 const MoreOption = () => {
-    const SheetRef = useRef(null)
+    const SheetRef = useRef(null);
     const option = [
-        { title: 'Language', Description: 'Your Selection', },
+        { title: 'Language', Description: 'Your Selection' },
         { title: 'Favourite Odhuvar', Description: 'Your Selection' },
         { title: 'Go to website', Description: 'Your Selection' },
         { title: 'Website search', Description: 'Your Selection' },
@@ -19,40 +28,40 @@ const MoreOption = () => {
         { title: 'Share the app', Description: 'Your Selection' },
         { title: 'Rate the app', Description: 'Your Selection' },
         { title: 'About', Description: 'Your Selection' },
-    ]
-    const [selectedLanguage, setSelectedLanguage] = useState(null)
+    ];
+    const [selectedLanguage, setSelectedLanguage] = useState(null);
     const navigationHandler = (item) => {
         if (item.title == 'Language') {
-            SheetRef.current.open()
+            SheetRef.current.open();
         }
-    }
+    };
+    const { theme } = useContext(ThemeContext);
     const rednderItem = (item, index) => {
         // console.log("🚀 ~ file: MoreOption.js:19 ~ rednderItem ~ item:", item)
         return (
-            <Pressable style={styles.list} onPress={() => navigationHandler(item)} >
+            <Pressable style={styles.list} onPress={() => navigationHandler(item)}>
                 <View style={{ flexDirection: 'row' }}>
-                    <View style={styles.imageContainer}>
-
-                    </View>
+                    <View style={styles.imageContainer}></View>
                     <View style={{ paddingHorizontal: 10 }}>
-                        <Text style={styles.titleText}>
+                        <Text style={[styles.titleText, { color: theme.textColor }]}>
                             {item.title}
                         </Text>
                         <Text style={{ color: colors.grey6 }}>{item.Description}</Text>
                     </View>
                 </View>
                 <TouchableOpacity style={{ paddingHorizontal: 10 }}>
-                    <Icon name='arrow-forward-ios' size={17} color={'#C1554E'} />
+                    <Icon name="arrow-forward-ios" size={17} color={'#C1554E'} />
                 </TouchableOpacity>
             </Pressable>
-        )
-    }
+        );
+    };
     return (
-        <View style={styles.main}>
+        <View style={[styles.main, { backgroundColor: theme.backgroundColor, flex: 1 }]}>
+            <Text>{theme.bgColor}</Text>
             <Background>
                 <ConstantHeader />
             </Background>
-            <View style={styles.container}>
+            <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
                 <FlatList
                     contentContainerStyle={{ marginTop: 20 }}
                     renderItem={({ item, index }) => rednderItem(item, index)}
@@ -70,12 +79,18 @@ const MoreOption = () => {
             </View>
         </View>
     );
-}
+};
 export const styles = StyleSheet.create({
-    main: {},
-    container: { backgroundColor: '#FFFFFF', },
-    list: { flexDirection: 'row', justifyContent: 'space-between', height: 45, width: '100%', marginBottom: 10 },
-    titleText: { fontSize: 14, fontFamily: 'Mulish-Bold', color: 'black', },
-    imageContainer: { backgroundColor: '#F2F0F8', width: 50, height: 45 }
-})
-export default MoreOption
+    main: { backgroundColor: 'green' },
+    container: {},
+    list: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: 45,
+        width: '100%',
+        marginBottom: 10,
+    },
+    titleText: { fontSize: 14, fontFamily: 'Mulish-Bold', color: 'black' },
+    imageContainer: { backgroundColor: '#F2F0F8', width: 50, height: 45 },
+});
+export default MoreOption;
