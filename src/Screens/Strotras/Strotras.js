ed4b1c8cc0658585208b515ed952de38d88eb75f
shawnmarkus <shawnmarkus4200@gmail.com>
2024-02-06 12:38:34 +0530

    page for strotras

---
+++ src/Screens/Strotras/Strotras.js
@@ -0,0 +1,8 @@
+import { Text } from 'react-native';
+import { useState } from 'react';
+
+const Strotas = () => {
+    return <Text style={{ color: 'black' }}>Strotas</Text>;
+};
+
+export default Strotas;
