e7b5723f7f8e540449c50f229b37bfa8e4318f8d
Kashifstar151 <kashifstar151@gmail.com>
2024-02-09 03:37:22 +0530

    Fav page UI and show data

--- src/Databases/AudioPlayerDatabase.js
+++ src/Databases/AudioPlayerDatabase.js
@@ -7,6 +7,7 @@ export async function AddSongToDatabase(query, body, callbacks) {
     audioPlayerDatabase.executeSql(sql, body, (result) => {
         Alert.alert("Success", "User created successfully.");
         console.log('audio created successfully.')
+        callbacks({ message: 'Success' })
     }, (error) => {
         console.log("Create user error", error);
     });
@@ -19,16 +20,39 @@ export const createUserTable = () => {
         console.log("Create table error", error)
     })
 }
-export const listfavAudios = async () => {
+export async function listfavAudios(callbacks) {
     let sql = "SELECT * FROM fav_odhuvar";
     audioPlayerDatabase.transaction((tx) => {
         tx.executeSql(sql, [], (tx, resultSet) => {
             var length = resultSet.rows.length;
+            let arr = []
             for (var i = 0; i < length; i++) {
                 console.log("row data", resultSet.rows.item(i));
+                arr.push(resultSet.rows.item(i))
             }
+            callbacks(arr)
         }, (error) => {
             console.log("List user error", error);
+            callbacks({ message: 'No data found' })
         })
     })
+}
+export const createDownloadTable = () => {
+    audioPlayerDatabase.executeSql("CREATE TABLE IF NOT EXISTS Downlaoded_audios (id INTEGER PRIMARY KEY AUTOINCREMENT, url VARCHAR, title VARCHAR , artist VARCHAR , thalamOdhuvarTamilname VARCHAR, categoryName VARCHAR, thirumariasiriyar VARCHAR )", [], (result) => {
+        console.log("Table created successfully");
+    }, (error) => {
+        console.log("Create table error", error)
+    })
+}
+export async function AddDownloadedAudios(query, body, callbacks) {
+    // console.log("🚀 ~ AddSongToDatabase ~ body:", body)
+    let sql = "INSERT INTO Downlaoded_audios (id,url,title,artist,thalamOdhuvarTamilname,categoryName,thirumariasiriyar) VALUES (?, ?, ?, ?, ?, ?, ?)"; //storing user data in an array
+    audioPlayerDatabase.executeSql(sql, body, (result) => {
+        Alert.alert("Success", "User created successfully.");
+        console.log('audio created successfully.')
+        callbacks({ message: 'Success' })
+    }, (error) => {
+        console.log("Create user error", error);
+    });
+    // }
 }
