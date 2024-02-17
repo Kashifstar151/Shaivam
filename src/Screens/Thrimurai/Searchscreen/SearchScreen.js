c494b30c309af3704c6d59befe62827af198f404
Kashifstar151 <kashifstar151@gmail.com>
2024-02-17 13:47:24 +0530

    code merge

--- src/Screens/Thrimurai/Searchscreen/SearchScreen.js
+++ src/Screens/Thrimurai/Searchscreen/SearchScreen.js
@@ -45,8 +45,7 @@ const SearchScreen = ({ navigation, route }) => {
 
         if (searchText && searchText.length >= 2) {
             getSqlData(
-                `SELECT * FROM thirumurais WHERE searchTitle LIKE '%${searchText}%' ${
-                    !fktrimuria.has(0) ? `and fkTrimuria IN (${[...fktrimuria].join(',')})` : ''
+                `SELECT * FROM thirumurais WHERE searchTitle LIKE '%${searchText}%' ${!fktrimuria.has(0) ? `and fkTrimuria IN (${[...fktrimuria].join(',')})` : ''
                 } GROUP BY titleS  ;`,
                 // `SELECT * FROM thirumurais WHERE search_title='%திருஞானசம்பந்தர்தேவாரம்-1.031-திருக்குரங்கணின்முட்டம்-விழுநீர்மழுவாள்படை%' LIMIT 10 OFFSET 0;`,
                 (callbacks) => {
@@ -54,8 +53,7 @@ const SearchScreen = ({ navigation, route }) => {
                 }
             );
             getSqlData(
-                `SELECT * FROM thirumurai_songs WHERE searchTitle LIKE '%${searchText}%' ${
-                    !fktrimuria.has(0) ? `and thirumuraiId IN (${[...fktrimuria].join(',')})` : ''
+                `SELECT * FROM thirumurai_songs WHERE searchTitle LIKE '%${searchText}%' ${!fktrimuria.has(0) ? `and thirumuraiId IN (${[...fktrimuria].join(',')})` : ''
                 } ORDER BY songNo ASC `,
                 (callbacks) => {
                     setRawSongs(callbacks);
@@ -230,26 +228,22 @@ const SearchScreen = ({ navigation, route }) => {
                                     }}
                                 >
                                     {`${item?.id === 0 ? 'All' : ''} `}
-                                    {`${
-                                        item?.id > 0 && item?.id < 8
-                                            ? `${t(`Thrimurai ${item?.id}`)}`
-                                            : ''
-                                    }`}
-                                    {`${
-                                        item?.id >= 8 && item?.id !== 10 && item?.id !== 11
-                                            ? `${t(nameMap[`Thrimurai ${item?.id}`])}`
-                                            : ''
-                                    }`}
-                                    {`${
-                                        item?.id === 10
-                                            ? `${t(nameMap[`Thrimurai ${item?.id}`]).split('/')[0]}`
-                                            : ''
-                                    }`}
-                                    {`${
-                                        item?.id === 11
-                                            ? `${t(nameMap[`Thrimurai ${item?.id}`]).split('/')[1]}`
-                                            : ''
-                                    }`}
+                                    {`${item?.id > 0 && item?.id < 8
+                                        ? `${t(`Thrimurai ${item?.id}`)}`
+                                        : ''
+                                        }`}
+                                    {`${item?.id >= 8 && item?.id !== 10 && item?.id !== 11
+                                        ? `${t(nameMap[`Thrimurai ${item?.id}`])}`
+                                        : ''
+                                        }`}
+                                    {`${item?.id === 10
+                                        ? `${t(nameMap[`Thrimurai ${item?.id}`]).split('/')[0]}`
+                                        : ''
+                                        }`}
+                                    {`${item?.id === 11
+                                        ? `${t(nameMap[`Thrimurai ${item?.id}`]).split('/')[1]}`
+                                        : ''
+                                        }`}
                                 </Text>
                             </TouchableOpacity>
                         )}
