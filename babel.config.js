3374647ffe5b48d5ed24b85f58f0e9f0254fc0ae
Kashifstar151 <kashifstar151@gmail.com>
2024-03-01 11:10:56 +0530

    local commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
 module.exports = {
-    presets: ['module:@react-native/babel-preset'],
+    presets: ['module:@react-native/babel-preset', 'react-native-dotenv'],
     plugins: ['react-native-reanimated/plugin'],
 };
