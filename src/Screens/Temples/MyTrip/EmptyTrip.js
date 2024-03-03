bcad54a81b1d8bc1bbd914433d264d9f26c058f1
Kashifstar151 <kashifstar151@gmail.com>
2024-03-03 15:27:09 +0530

    Empty Trip UI

--- src/Screens/Temples/MyTrip/EmptyTrip.js
+++ src/Screens/Temples/MyTrip/EmptyTrip.js
@@ -6,7 +6,11 @@ const EmptyTrip = () => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Icon />
-            <Text style={{ color: 'black' }}>Plan your first trip now!</Text>
+            <View style={{ marginTop: 10, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ color: 'black', fontSize: 18, fontFamily: 'Mulish-Bold' }}>Plan your first trip now!</Text>
+                <Text style={{ color: 'black', fontSize: 14, }}>Follow the simple steps and start your trip</Text>
+            </View>
+
         </View>
     );
 };
