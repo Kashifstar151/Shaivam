5adde6946f0320cb0401314ac98c74d02a59a634
shivam <shivam@bytive.in>
2024-03-06 12:28:40 +0530

    removing irrelevent line  adn storing the files at assets

--- src/Screens/Temples/NearByTemples.js
+++ src/Screens/Temples/NearByTemples.js
@@ -5,55 +5,6 @@ import Slider from '@react-native-community/slider';
 import SearchInput from '../../components/SearchInput';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import { ScrollView } from 'react-native-gesture-handler';
-
-/*
-
-<TouchableOpacity
-                onPress={() => {
-                    console.log('the click');
-                    return snapToIndex(2);
-                }}
-            >
-                <View>
-                    <Text>Click me</Text>
-                </View>
-            </TouchableOpacity>
-
-            <Text style={{ color: 'black', fontWeight: 700, }}>Near by Temples</Text>
-            <FlatList horizontal
-                contentContainerStyle={{ backgroundColor: 'red' }}
-                data={[1, 2, 3, 4, 5, 6, 788, 77, 88]}
-                renderItem={(i) => (
-                    <Text
-                        style={{
-                            color: '#000',
-                            marginHorizontal: 20,
-                            backgroundColor: 'yellow',
-                        }}>
-                        {JSON.stringify(i)}
-                    </Text>
-                )}
-                keyExtractor={(item, ind) => ind}/>
-
-            <Slider
-                value={position}
-                onValueChange={(value) => setPosition(value)}
-                onSlidingComplete={(value) => setPosition(value)}
-                style={{
-                    width: Dimensions.get('window').width - 30,
-                    alignSelf: 'center',
-                    marginVertical: 100,
-                }}
-                minimumValue={0}
-                maximumValue={100}
-                minimumTrackTintColor="#C1554E"
-                maximumTrackTintColor="#EFEFEF"
-                thumbTintColor="#C1554E"
-            />
-
-
-*/
-
 const NearByTemples = ({ close, data, locationName }) => {
     const { snapToIndex, snapToPosition } = useBottomSheet();
     const [position, setPosition] = useState(0);
@@ -68,14 +19,12 @@ const NearByTemples = ({ close, data, locationName }) => {
             </View>
 
             <View>
-                <Text > Nearby Temples in {locationName}</Text>
+                <Text> Nearby Temples in {locationName}</Text>
             </View>
 
             <ScrollView scrollIndicatorInsets={true}>
                 {data.map((item, _) => (
-                    <Text key={_}>
-                        {JSON.stringify(item)}
-                    </Text>
+                    <Text key={_}>{JSON.stringify(item)}</Text>
                 ))}
             </ScrollView>
         </View>
--- src/Screens/Temples/Temples.js
+++ src/Screens/Temples/Temples.js
@@ -1,12 +1,25 @@
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { View, Text, StyleSheet, Dimensions, TouchableOpacity, PermissionsAndroid } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity,
+    PermissionsAndroid,
+} from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import NearByTemples from './NearByTemples';
 import { RouteTexts } from '../../navigation/RouteText';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Image } from 'react-native';
-import { clearGetCurrentLocationWatcher, getCurrentLocationWatcher, getTheLocationName, locationPermission, onRegionChangeCompleteCallback } from '../../Helpers/GeolocationFunc';
+import {
+    clearGetCurrentLocationWatcher,
+    getCurrentLocationWatcher,
+    getTheLocationName,
+    locationPermission,
+    onRegionChangeCompleteCallback,
+} from '../../Helpers/GeolocationFunc';
 
 /*
 
@@ -25,79 +38,74 @@ const assetMapWithTempleType = {
     1: {
         name: 'Thirumurai Temple',
         size: {},
-        path: require('./ThirumuraiTemple.png'),
+        path: require('./TempleAssets/ThirumuraiTemple.png'),
         metaData: {
-            color: "#E62828"
-        }
+            color: '#E62828',
+        },
     },
     2: {
         name: 'Temples',
         size: {},
         metaData: {
-            color: "#6EDB00"
+            color: '#6EDB00',
         },
-        path: require('./Temples.png'),
+        path: require('./TempleAssets/Temples.png'),
     },
     3: {
         name: 'Popular Temple',
         size: {},
         metaData: {
-            color: "#007DE6"
+            color: '#007DE6',
         },
-        path: require('./PopularTemples.png'),
+        path: require('./TempleAssets/PopularTemples.png'),
     },
 
     4: {
         name: 'Parashurama Temple',
         size: {},
         metaData: {
-            color: "#D700C1"
+            color: '#D700C1',
         },
-        path: require('./ParashuramaTemple.png'),
+        path: require('./TempleAssets/ParashuramaTemple.png'),
     },
     5: {
         name: 'Mukti Sthalam Temple',
         size: {},
         metaData: {
-            color: "#D700C1"
+            color: '#D700C1',
         },
-        path: require('./MuktiSthalamTemple.png'),
+        path: require('./TempleAssets/MuktiSthalamTemple.png'),
     },
     6: {
         name: 'Unknown Temple',
         size: {},
         metaData: {
-            color: "#A5A5A5"
+            color: '#A5A5A5',
         },
-        path: require('./UnknownTemple.png'),
+        path: require('./TempleAssets/UnknownTemple.png'),
     },
     7: {
         name: 'User Location',
         size: {},
         metaData: {
-            color: "#f00"
+            color: '#f00',
         },
-        path: require('./UsersLocation.png'),
+        path: require('./TempleAssets/UsersLocation.png'),
     },
     8: {
         name: 'Vaippu Sthalam Temple',
         size: {},
         metaData: {
-            color: "#D700C1"
+            color: '#D700C1',
         },
-        path: require('./VaippuSthalamTemple.png'),
+        path: require('./TempleAssets/VaippuSthalamTemple.png'),
     },
 };
 
 export const CustomMarker = ({ flag, coordinate }) => {
     return (
-        <Marker
-            coordinate={coordinate}
-
-            description={'This is a marker in React Natve'}
-        >
+        <Marker coordinate={coordinate} description={'This is a marker in React Natve'}>
             <Image source={assetMapWithTempleType[flag].path} resizeMode="contain" />
-
         </Marker>
     );
 };
@@ -113,8 +121,8 @@ export const Temples = () => {
         longitude: 77.387901,
         latitudeDelta: 0.015,
         longitudeDelta: 0.0121,
-        locationName: ""
-    })
+        locationName: '',
+    });
 
     // callbacks
     const handleSheetChanges = useCallback((index) => {
@@ -123,77 +131,72 @@ export const Temples = () => {
 
     const [nearByTempleList, setNearByTempleList] = useState([
         {
-            name: "Ambey ma temple",
-            flag: "",
+            name: 'Ambey ma temple',
+            flag: '',
             metadata: () => {
-                return assetMapWithTempleType[flag]
-            }
-        }
-    ])
+                return assetMapWithTempleType[flag];
+            },
+        },
+    ]);
 
-    const [padState, setPadState] = useState(1)
+    const [padState, setPadState] = useState(1);
 
     useEffect(() => {
         getCurrentLocationWatcher((val) => {
-            console.log("the value is set for map ")
-            setRegionCoordinate(prev => ({ ...prev, ...val }))
-        })
-        return () => clearGetCurrentLocationWatcher()
-    }, [])
+            console.log('the value is set for map ');
+            setRegionCoordinate((prev) => ({ ...prev, ...val }));
+        });
+        return () => clearGetCurrentLocationWatcher();
+    }, []);
 
     useEffect(() => {
-
         (async () => {
-            await locationPermission()
-        })()
-
+            await locationPermission();
+        })();
 
         setTimeout(() => {
-            setPadState(0)
-        }, 100)
+            setPadState(0);
+        }, 100);
+    }, [padState]);
 
-
-    }, [padState])
-
-    const [userLocName, setUserLocName] = useState("")
-
+    const [userLocName, setUserLocName] = useState('');
 
     useEffect(() => {
-        console.log("the format=jsonv2& and coord==>", regionCoordinate)
+        console.log('the format=jsonv2& and coord==>', regionCoordinate);
         if (regionCoordinate?.latitude && regionCoordinate?.longitude) {
             (async () => {
-                const locationDetail = await getTheLocationName({ ...regionCoordinate })
+                const locationDetail = await getTheLocationName({ ...regionCoordinate });
 
-                console.log("the location of the user ==>", locationDetail?.display_name)
-                setUserLocName(prev => {
-                    return locationDetail?.address?.village || locationDetail?.name || locationDetail?.display_name
-                })
-            })()
+                console.log('the location of the user ==>', locationDetail?.display_name);
+                setUserLocName((prev) => {
+                    return (
+                        locationDetail?.address?.village ||
+                        locationDetail?.name ||
+                        locationDetail?.display_name
+                    );
+                });
+            })();
         }
-    }, [regionCoordinate])
-
-
-
+    }, [regionCoordinate]);
 
     const onMapReadyCallback = async () => {
-        // console.log("the location permission------------------------------------------------------------------------------")
-        const state = await locationPermission()
+        const state = await locationPermission();
 
         if (state.status) {
             getCurrentLocationWatcher((val) => {
-                console.log("the value is set for map ")
-                setRegionCoordinate(prev => ({ ...prev, ...val }))
-            })
+                console.log('the value is set for map ');
+                setRegionCoordinate((prev) => ({ ...prev, ...val }));
+            });
         }
-    }
+    };
 
     // useEffect(() => {
     //     console.log("the region coors have been changed ==>", viewAreaCoors)
     // }, [viewAreaCoors])
 
     const [viewAreaCoors, setViewAreaCoors] = useState({
-        ...regionCoordinate
-    })
+        ...regionCoordinate,
+    });
     return (
         <View style={{ flex: 1 }}>
             <MapView
@@ -202,16 +205,20 @@ export const Temples = () => {
                 style={styles.map}
                 showsUserLocation={true}
                 showsMyLocationButton={true}
-                onRegionChangeComplete={args => onRegionChangeCompleteCallback(args, setViewAreaCoors)}
+                onRegionChangeComplete={(args) =>
+                    onRegionChangeCompleteCallback(args, setViewAreaCoors)
+                }
                 // onRegionChange={onRegionChangeCallback}
                 // mapPadding={{ top: 0, right: 0, bottom: 700, left: 0 }}
                 region={regionCoordinate}
             >
-                <CustomMarker flag={4} coordinate={{
-                    latitude: 28.500271,
-                    longitude: 77.387901,
-
-                }} />
+                <CustomMarker
+                    flag={4}
+                    coordinate={{
+                        latitude: 28.500271,
+                        longitude: 77.387901,
+                    }}
+                />
                 <CustomMarker flag={7} coordinate={regionCoordinate} />
             </MapView>
 
@@ -221,7 +228,11 @@ export const Temples = () => {
                 // containerHeight={400}
                 snapPoints={['15%', '95%']}
             >
-                <NearByTemples locationName={userLocName} data={nearByTempleList} close={() => bottomSheetRef.current.snapToIndex(0)} />
+                <NearByTemples
+                    locationName={userLocName}
+                    data={nearByTempleList}
+                    close={() => bottomSheetRef.current.snapToIndex(0)}
+                />
             </BottomSheet>
         </View>
     );
