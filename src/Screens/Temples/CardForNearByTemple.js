ba6a40eab52413b84a1b16fc65527a5b916e40ef
shivam <shivam@bytive.in>
2024-03-07 18:58:52 +0530

    mojor task of the nearby page is done

--- src/Screens/Temples/CardForNearByTemple.js
+++ src/Screens/Temples/CardForNearByTemple.js
@@ -1,41 +1,74 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FavSVG from '../../components/SVGs/FavSVG';
 
 const CardForNearByTemple = ({ item }) => {
+    const [favState, setFavState] = useState(false);
     return (
-        <View
-            style={{
-                backgroundColor: 'white',
-                marginHorizontal: 15,
-                marginTop: 15,
-                elevation: 5,
-                shadowOffset: {
-                    width: 5,
-                    height: 5,
-                },
-                borderRadius: 8,
-                height: 70,
-                padding: 10,
-            }}
-        >
-            <View style={{ flex: 1 }}>
-                <View style={{ flexDirection: 'row', flex: 1, gap: 10 }}>
+        <View style={styles.topWrapperForCard}>
+            <View style={styles.flexRow}>
+                <View style={[styles.flexRow, { gap: 8 }]}>
                     <View
-                        style={{
-                            width: 4,
-                            backgroundColor: `${item?.metadata().color}`,
-                            borderRadius: 5,
-                        }}
+                        style={[
+                            styles.widthForColorBar,
+                            {
+                                backgroundColor: `${item?.metadata().color}`,
+                            },
+                        ]}
                     ></View>
-                    <View style={{ marginLeft: 10 }}>
-                        <Text style={{ color: 'red', fontWeight: '' }}>
-                            {JSON.stringify(item?.metadata().color)}
-                        </Text>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.textBold}>SHIvam</Text>
+                        <Text style={{ color: 'black' }}>Sharma</Text>
                     </View>
+                </View>
+
+                <View style={styles.btnWrapper}>
+                    <Pressable
+                        onPress={() => {
+                            setFavState(!favState);
+                        }}
+                    >
+                        {/* <Text style={styles.btn}>FAV</Text>
+                         */}
+                        <FavSVG fill={'#000'} />
+                    </Pressable>
                 </View>
             </View>
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    topWrapperForCard: {
+        backgroundColor: 'white',
+        marginHorizontal: 15,
+        marginTop: 15,
+        elevation: 5,
+        shadowOffset: {
+            width: 5,
+            height: 5,
+        },
+        borderRadius: 8,
+        height: 70,
+        padding: 10,
+    },
+
+    flexRow: { flex: 1, flexDirection: 'row' },
+    widthForColorBar: {
+        width: 4,
+        borderRadius: 5,
+    },
+    textContainer: { marginLeft: 10, flex: 1, justifyContent: 'center' },
+    textBold: {
+        color: 'black',
+        fontFamily: 'Mulish-Bold',
+        fontSize: 14,
+    },
+    btnWrapper: { marginRight: 8, justifyContent: 'center' },
+    btn: {
+        color: 'red',
+        fontSize: 12,
+        margin: 10,
+    },
+});
 export default CardForNearByTemple;
