6208b22d40cd26103478be5123ca4277d2a50930
shivam <shivam@bytive.in>
2024-04-04 16:30:56 +0530

    added files for parts in home page

---
+++ src/Screens/Home/VideosList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const VideosList = () => {
+    return (
+        <View>
+            <Text>VideosList</Text>
+        </View>
+    );
+};
+
+export default VideosList;
