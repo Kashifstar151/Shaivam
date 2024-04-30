b21372386770634c45640bf90ef17766913efc72
shivam <shivam@bytive.in>
2024-04-30 12:49:15 +0530

    added initial db info

---
+++ DBInfo.js
@@ -0,0 +1,9 @@
+export default {
+    DumpName: 'thirumuraiSongs',
+    FilePath:
+        'https://shaivamfiles.fra1.cdn.digitaloceanspaces.com/sqlitedump/thirumuraiSongs_12.zip',
+    Version: '12',
+    createdAt: '2024-04-09T10:17:27.323Z',
+    publishedAt: '2024-04-09T10:17:28.545Z',
+    updatedAt: '2024-04-29T07:08:10.139Z',
+};
