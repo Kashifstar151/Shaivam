f661d3f7a9123d2db88bc23749015a6d2dc23e3f
shivam <shivam@bytive.in>
2024-05-03 21:36:31 +0530

    marker with callout has been updated

--- src/Screens/Temples/CustomMarker.js
+++ src/Screens/Temples/CustomMarker.js
@@ -1,27 +1,52 @@
 import { Image, Text, View } from 'react-native';
 import { Callout, Marker } from 'react-native-maps';
 import assetMapWithTempleType from './AssetMapWithTempleType';
-export const MarkerCallOut = ({ setPadState, callback, flag, coordinate, keyName, children }) => {
+export const MarkerCallOut = ({ callback, flag, coordinate, description }) => {
     return (
         <Marker
             tracksViewChanges={false}
             coordinate={coordinate}
             description={'This is a marker in React Natve'}
-            onPress={callback ? callback : null}
-        // image={assetMapWithTempleType[1].path}
+            // image={assetMapWithTempleType[1].path}
+            style={{
+                width: '100%',
+                alignItems: 'center',
+            }}
         >
-            <Image
-                onLoadEnd={() => {
-                    setTimeout(() => {
-                        setPadState((prev) => !prev);
-                    }, 1000);
-                }}
-                source={assetMapWithTempleType[flag].path}
-                resizeMode="contain"
-                key={`${keyName}-${flag}`}
-            />
-            <Callout>
-                <Text style={{ color: 'red', backgroundColor: 'black' }}>Suck my dickk </Text>
+            <View>{assetMapWithTempleType[flag]?.Svg}</View>
+            <Callout
+                tooltip={true}
+                onPress={callback ? callback : null}
+                style={{ alignItems: 'center' }}
+            >
+                <View
+                    style={{
+                        backgroundColor: 'black',
+                        borderRadius: 10,
+                        paddingVertical: 10,
+                        paddingHorizontal: 20,
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: 'white',
+                        }}
+                    >
+                        {description ?? 'Sorry no data available'}
+                    </Text>
+                </View>
+                <View
+                    style={{
+                        borderTopWidth: 10,
+                        width: 20,
+                        height: 10,
+                        borderLeftWidth: 10,
+                        borderRightWidth: 10,
+                        borderRightColor: 'transparent',
+                        borderLeftColor: 'transparent',
+                        borderTopColor: 'black',
+                    }}
+                ></View>
             </Callout>
         </Marker>
     );
@@ -68,7 +93,7 @@ export const CustomMarker = ({ setPadState, callback, flag, coordinate, keyName,
             coordinate={coordinate}
             description={'This is a marker in React Natve'}
             onPress={callback ? callback : null}
-        // image={assetMapWithTempleType[1].path}
+            // image={assetMapWithTempleType[1].path}
         >
             {assetMapWithTempleType[flag]?.Svg}
             {/* <Image
