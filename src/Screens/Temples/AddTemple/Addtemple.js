233947614500a40096db736e3124924d114276de
shivam <shivam@bytive.in>
2024-05-09 06:12:29 +0530

    added nav props and refoctored code

--- src/Screens/Temples/AddTemple/Addtemple.js
+++ src/Screens/Temples/AddTemple/Addtemple.js
@@ -3,11 +3,11 @@ import { RefreshControl, StyleSheet, Text, View } from 'react-native';
 import Background from '../../../components/Background';
 import TempleHeader from '../TempleHeader';
 import { FlatList } from 'react-native';
-import AddTempleHomePageLogo from '../../../../assets/Images/Temple/AddTempleHomePage.svg';
 import { RFValue } from 'react-native-responsive-fontsize';
-import { CustomLongBtn } from '../../../components/Buttons';
+import { EmptyList } from './EmptyList';
+import { RenderCardForAddTemple } from './RenderCardForAddTemple';
 
-const Addtemple = () => {
+const Addtemple = ({ navigation }) => {
     // const data = [1, 2, 3, 4, 5, 6];
     const data = [];
 
@@ -50,83 +50,19 @@ const Addtemple = () => {
                     contentContainerStyle={{
                         flexGrow: 1,
                     }}
-                    renderItem={({ item, index }) => (
-                        <View>
-                            <Text>{item}</Text>
-                        </View>
-                    )}
+                    renderItem={({ item, index }) => <RenderCardForAddTemple item={item} />}
                     refreshControl={
                         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
                     }
-                    ListEmptyComponent={() => <EmptyList onPressOfBtn={() => {}} />}
+                    ListEmptyComponent={() => (
+                        <EmptyList onPressOfBtn={() => {}} navigation={navigation} />
+                    )}
                 />
             </View>
         </View>
     );
 };
 
-const EmptyList = ({ onPressOfBtn }) => (
-    <View
-        style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: -50,
-        }}
-    >
-        <AddTempleHomePageLogo />
-        <View
-            style={{
-                paddingTop: 15,
-                width: '90%',
-                gap: 4,
-            }}
-        >
-            <Text style={[style.emptyListTextColor, style.emptyListHeading]}>
-                Submit your first entry here
-            </Text>
-            <Text style={[style.emptyListTextColor, style.emptyListSubHeading]}>
-                Once submitted, our admin will look into it and the add it into the database
-            </Text>
-        </View>
-
-        <View
-            style={{
-                width: '100%',
-                paddingTop: 15,
-            }}
-        >
-            <CustomLongBtn
-                onPress={onPressOfBtn}
-                text={'Add a Temple'}
-                textStyle={{
-                    color: '#4C3600',
-                    fontFamily: 'Mulish-Bold',
-                }}
-                containerStyle={{
-                    backgroundColor: '#FCB300',
-                }}
-            />
-        </View>
-    </View>
-);
-
-const style = StyleSheet.create({
-    emptyListHeading: {
-        fontFamily: 'Mulish-Bold',
-        fontSize: RFValue(18, 850),
-    },
-    emptyListTextColor: {
-        textAlign: 'center',
-        color: '#000',
-    },
-    emptyListSubHeading: {
-        fontFamily: 'Mulish',
-        fontSize: RFValue(14, 850),
-        textAlign: 'center',
-        color: '#000',
-        lineHeight: 18,
-    },
-});
+const style = StyleSheet.create({});
 
 export default Addtemple;
