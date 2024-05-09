83026ce7ff7c40fe4e466405cbc5784e6acafa10
shivam <shivam@bytive.in>
2024-05-10 00:45:14 +0530

    added all add temple related form under modal

--- src/Screens/Temples/AddTemple/Addtemple.js
+++ src/Screens/Temples/AddTemple/Addtemple.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { RefreshControl, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Modal, RefreshControl, StyleSheet, Text, View } from 'react-native';
 import Background from '../../../components/Background';
 import TempleHeader from '../TempleHeader';
 import { FlatList } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { EmptyList } from './EmptyList';
 import { RenderCardForAddTemple } from './RenderCardForAddTemple';
+import ModalComponent from './ModalComponent';
+import AddTempleForm from './AddTempleForm';
+import SelectLocation from './SelectLocation';
+import PreviewPage from './PreviewPage';
 
+const initialStepVal = {
+    first: 'false',
+    second: 'false',
+    third: 'false',
+};
 const Addtemple = ({ navigation }) => {
     // const data = [1, 2, 3, 4, 5, 6];
     const data = [];
+    // for stepper form
+    const [step, setStep] = useState(initialStepVal);
 
     // for refresh
     const [refreshing, setRefreshing] = React.useState(false);
@@ -19,6 +30,36 @@ const Addtemple = ({ navigation }) => {
             setRefreshing(false);
         }, 2000);
     }, []);
+
+    const setTheFlagsForModal = (num) => {
+        {
+            switch (num) {
+                case 1:
+                    setStep(() => ({
+                        ...initialStepVal,
+                        first: true,
+                    }));
+                    break;
+                case 2:
+                    setStep(() => ({
+                        ...initialStepVal,
+                        second: true,
+                    }));
+                    break;
+
+                case 3:
+                    setStep(() => ({
+                        ...initialStepVal,
+                        third: true,
+                    }));
+                    break;
+                default:
+                    setStep(() => ({
+                        ...initialStepVal,
+                    }));
+            }
+        }
+    };
     return (
         <View style={{ flex: 1 }}>
             <Background force={'light'}>
@@ -55,10 +96,36 @@ const Addtemple = ({ navigation }) => {
                         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
                     }
                     ListEmptyComponent={() => (
-                        <EmptyList onPressOfBtn={() => {}} navigation={navigation} />
+                        <EmptyList
+                            onPressOfBtn={() => {
+                                setStep((prev) => ({
+                                    ...prev,
+                                    first: true,
+                                }));
+                            }}
+                            navigation={navigation}
+                        />
                     )}
                 />
             </View>
+
+            <ModalComponent isVisible={step.first}>
+                <AddTempleForm
+                    navigation={navigation}
+                    setStep={(num) => setTheFlagsForModal(num)}
+                />
+            </ModalComponent>
+
+            <ModalComponent isVisible={step.second}>
+                <SelectLocation
+                    navigation={navigation}
+                    setStep={(num) => setTheFlagsForModal(num)}
+                />
+            </ModalComponent>
+
+            <ModalComponent isVisible={step.third}>
+                <PreviewPage navigation={navigation} setStep={(num) => setTheFlagsForModal(num)} />
+            </ModalComponent>
         </View>
     );
 };
