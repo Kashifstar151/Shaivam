a0a755d918aaee797138be77c07bd0b67fce615d
shivam <shivam@bytive.in>
2024-05-10 10:56:05 +0530

    added a raw card for temple module

---
+++ src/Screens/Temples/AddTemple/RenderCardForAddTemple.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export const RenderCardForAddTemple = ({ item }) => {
+    return (
+        <View>
+            <Text>RenderCardForAddTemple {item}</Text>
+        </View>
+    );
+};
