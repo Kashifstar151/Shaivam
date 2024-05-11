f86b99e20c99030d2436b44fd2a79a9ac84ca2a7
shivam <shivam@bytive.in>
2024-05-11 23:12:36 +0530

    increased the radius and changed the name

--- src/store/features/Temple/TemplApiSlice.js
+++ src/store/features/Temple/TemplApiSlice.js
@@ -1,13 +1,13 @@
 import { TempleApiSlice } from '../../apiSlice';
 
-const CalenderApiSlice = TempleApiSlice.injectEndpoints({
+const TempleApiSliceCall = TempleApiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getNearByTemples: builder.query({
             query: (date) => {
                 console.log(date, 'date from calender');
                 // const url = `?temple_coordinates[coords]=${date?.longitude},${date?.latitude}&pagination[pageSize]=200`;
                 // https://lobster-app-gpfv5.ondigitalocean.app/api/nearby-temples?long=77.391029&lat=28.535517&radius=15000
-                const url = `api/nearby-temples?long=${date?.longitude}&lat=${date?.latitude}&radius=3000`
+                const url = `api/nearby-temples?long=${date?.longitude}&lat=${date?.latitude}&radius=15000`;
                 console.log('🚀 ~ url:', url);
                 return {
                     url: url,
@@ -19,4 +19,4 @@ const CalenderApiSlice = TempleApiSlice.injectEndpoints({
     }),
 });
 
-export const { useGetNearByTemplesQuery } = CalenderApiSlice;
+export const { useGetNearByTemplesQuery } = TempleApiSliceCall;
