a7f298d253dd53d229dbe656fe3a824d6ebe86d0
Kashifstar151 <kashifstar151@gmail.com>
2024-06-26 17:54:25 +0530

    fix search page crash for note 5 pro , most searched location , autoplay when next and  prev , copy paste only working for starting lyrics fix

--- DBInfo.js
+++ DBInfo.js
@@ -1,8 +1,8 @@
 export default {
     DumpName: 'thirumuraiSongs',
     FilePath:
-        'https://shaivamfiles.fra1.cdn.digitaloceanspaces.com/sqlitedump/thirumuraiSong_27.zip',
-    Version: '27',
+        'https://shaivamfiles.fra1.cdn.digitaloceanspaces.com/sqlitedump/thirumuraiSong_29.zip',
+    Version: '29',
     createdAt: '2024-04-09T10:17:27.323Z',
     publishedAt: '2024-04-09T10:17:28.545Z',
     updatedAt: '2024-06-23T07:08:10.139Z',
